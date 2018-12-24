06e5e778f6eb51d4d87443f42fb3c72044d78f5c
sirusdynamo <sirusdynamo@gmail.com>
2018-12-24 21:50:30 +0100

    bug:problem with javacript code will fix later

--- myscript.js
+++ myscript.js
@@ -1,9 +1,9 @@
 var player1 =prompt("Player one: Enter your name you will be blue")
-var player1color ='rgb(86,151,255)'
+var player1color ='rgb(86, 151, 255)'
 
 
 var player2 =prompt("Player two: Enter your name you will be blue")
-var player1color ='rgb(237,45,73)'
+var player2color ='rgb(237, 45, 73)'
 
 
 var game_on =true;
@@ -45,14 +45,14 @@ function checkBottom(colIndex){
 // that is 4 horizontal,vertical or diagonal objects of
 // the same colour immediately adjacent to another in straigt line.
 function colorMatchCheck(one,two,three,four){
-    return(one === two && one !== two && one ===  three && one ===four && one === 'rgb(128, 128, 128)')
+    return(one === two && one !== undefined && one ===  three && one ===four && one === 'rgb(128, 128, 128)')
 
 }
 // check for horizonta wins
 function horizontalWinCheck(){
     for (var row =0; row<6; row++){
         for (var col = 0; col <4; col++){
-            if(colorMatchCheck(checkColor(row, col),checkColor(row, col+1),checkColor(row, col+2),checkColor(row, col+4))){
+            if(colorMatchCheck(checkColor(row, col),checkColor(row, col+1),checkColor(row, col+2),checkColor(row, col+3))){
                 console.log("horiz");
                 reportWin(row, col);
                 return true
@@ -106,3 +106,43 @@ function diagonalWinCheck(){
         
     }
 }
+
+
+var currentPlayer = 1;
+var currentName = player1;
+var currentColor = player1color;
+
+$('h3').text(player1+" It is your, pick a column to drop in!");
+
+$('.board button').on('click',function(){
+
+    var col = $(this).closest('td').index();
+
+    var bottomAvail =checkBottom(col);
+    
+    changeColor(bottomAvail,col,currentColor);
+    if(horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck()){
+        $('h1').text( currentName+"<b> You have won </b>");
+        $('h2').fadeOut('fast');
+        $('h3').fadeOut('fast');
+
+
+    }
+    
+    currentPlayer = currentPlayer * -1;
+
+    if (currentPlayer===1){
+        currentName =player1;
+        $('h3').text(currentName+" it is your turn");
+        currentColor=player1color;
+    }
+    else{
+        currentName =player2;
+        $('h3').text(currentName+" it is your turn");
+        currentColor=player2color;
+    }
+
+
+})
+
+ 
